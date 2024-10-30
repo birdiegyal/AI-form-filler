124117e3faf594eb744325ae563f883d79d23227
birdiegyal <birdiegyal@gmail.com>
2024-10-30 16:49:07 +0530

    i do need to understand how tailwind.config works someday.

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/routes/**/*.tsx"
+  ],
+  // i need to know what the fuck is this.
+  theme: {
+    extend: {},
+  },
+  // plus this as well.
+  plugins: [],
+}
+
